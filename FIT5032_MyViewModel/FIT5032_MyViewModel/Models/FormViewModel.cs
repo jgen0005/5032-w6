5e5d34d02d3c7d86267f7acf48acca2b3e7adb4b
jgen0005 <134963031+jgen0005@users.noreply.github.com>
2023-09-03 14:17:50 +0800

    studio 6 task

---
+++ FIT5032_MyViewModel/FIT5032_MyViewModel/Models/FormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FIT5032_MyViewModel.Models
+{
+    public class FormViewModel
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+    }
+}
